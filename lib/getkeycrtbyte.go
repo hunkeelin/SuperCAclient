deeb21aeabd309610309ed468530d82f9e3430c4
bgops <bgops@test1.klin-pro.com>
2018-07-30 21:59:45 -0700

    updated

--- lib/getkeycrtbyte.go
+++ lib/getkeycrtbyte.go
@@ -1,19 +1,21 @@
 package client
 
 import (
+	"bytes"
 	"encoding/pem"
 	"github.com/hunkeelin/klinutils"
 	"github.com/hunkeelin/pki"
 )
 
-func Getkeycrtbyte(w WriteInfo) (crtpem [][]byte, keypem []byte, err error) {
+func Getkeycrtbyte(w WriteInfo) (crtpem []byte, keypem []byte, err error) {
 	var crt [][]byte
+	bcrt := bytes.NewBufferString("")
 	csr, key := klinpki.GenCSRv2(w.CSRConfig)
 
 	masteraddr := klinutils.GetHostnameFromCert(w.CA)
 	f, err := Getcrtv2(w.CA, masteraddr, w.CAport, csr.Bytes)
 	if err != nil {
-		return crt, key.Bytes, err
+		return bcrt.Bytes(), key.Bytes, err
 	}
 	crt = append(crt, f.Cert)
 	if w.Chain {
@@ -24,5 +26,8 @@ func Getkeycrtbyte(w WriteInfo) (crtpem [][]byte, keypem []byte, err error) {
 			crt = append(crt, p.Bytes)
 		}
 	}
-	return crt, key.Bytes, nil
+	for _, c := range crt {
+		pem.Encode(bcrt, &pem.Block{Type: "CERTIFICATE", Bytes: c})
+	}
+	return bcrt.Bytes(), key.Bytes, nil
 }
--- lib/write.go
+++ lib/write.go
@@ -16,14 +16,7 @@ func Writecrtkeyv2(w WriteInfo) error {
 	if err != nil {
 		return err
 	}
-	for _, c := range crt {
-		pem.Encode(clientCRTFile, &pem.Block{Type: "CERTIFICATE", Bytes: c})
-		//if i == 0 {
-		//	pem.Encode(clientCRTFile, &pem.Block{Type: "CERTIFICATE", Bytes: c})
-		//} else {
-		//	clientCRTFile.Write(c)
-		//}
-	}
+	clientCRTFile.Write(crt)
 	clientCRTFile.Close()
 
 	keyOut, err := os.OpenFile(w.Path+".key", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
